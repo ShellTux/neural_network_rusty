0aef3b1cd0c2995cd3a69a3553a8c0e0c7e01d5a
ShellTux <115948079+ShellTux@users.noreply.github.com>
2025-03-28 11:32:24 +0000

    add perceptron visualization (WIP)

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ fn main() {
     let (mut rl, thread) = raylib::init()
         .size(800, 600)
         .title("Matrix Visualization")
+        .resizable()
         .log_level(TraceLogLevel::LOG_NONE)
         .build();
 
@@ -22,17 +23,6 @@ fn main() {
     let matrix = Matrix::random(5, 5, (min_value, max_value));
 
     let mut phi: f32 = 0.;
-
-    while !rl.window_should_close() {
-        let mut d = rl.begin_drawing(&thread);
-        d.clear_background(Color::BLACK);
-
-        matrix
-            .map(|x| x * (phi * PI).sin())
-            .visualize(&mut d, min_value, max_value);
-
-        phi += 0.01;
-    }
 
     let mut perceptron = Perceptron::new(2, 0.1);
 
@@ -52,8 +42,31 @@ fn main() {
 
     perceptron.train(&inputs, &labels, 10);
 
-    for input in inputs {
+    for input in &inputs {
         let prediction = perceptron.predict(&input);
         println!("Input: {:?}, Prediction: {}", input.data(), prediction);
     }
+
+    while !rl.window_should_close() {
+        let mut d = rl.begin_drawing(&thread);
+        d.clear_background(Color::BLACK);
+
+        //matrix
+        //    .map(|x| x * (phi * PI).sin())
+        //    .visualize(&mut d, min_value, max_value);
+
+        let input = Matrix::<f32>::new(1, 2).map(|_| (phi * PI).sin().abs());
+        let prediction = perceptron.predict(&input);
+
+        print!(
+            "{:?} | prediction = {prediction}            \r",
+            input.data()
+        );
+
+        perceptron.visualize(&mut d, &input);
+
+        phi += 0.01;
+    }
+
+    println!();
 }
