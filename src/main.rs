520941dd77e0086d63ff41fcc65d435e12c26eed
ShellTux <115948079+ShellTux@users.noreply.github.com>
2025-03-28 00:21:04 +0000

    chore(main.rs): matrix visualization dynamic

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,34 @@
 mod matrix;
 
+use std::f32::consts::PI;
+
 use matrix::Matrix;
 use raylib::prelude::*;
 
 #[allow(unused_variables)]
 fn main() {
-    let a: Matrix<usize> = Matrix::random(1, 2, (1, 10));
-    let b: Matrix<usize> = Matrix::random(2, 3, (1, 10));
-
-    println!("A:");
-    a.print();
-    println!("B:");
-    b.print();
-    println!();
-    a.multiply(&b).unwrap().print();
-
     let (mut rl, thread) = raylib::init()
         .size(800, 600)
         .title("Matrix Visualization")
+        .log_level(TraceLogLevel::LOG_NONE)
         .build();
 
-    let min_value: f32 = 0.;
-    let max_value: f32 = 100.;
+    rl.set_target_fps(60);
+
+    let min_value: f32 = -1.;
+    let max_value: f32 = 1.;
     let matrix = Matrix::random(5, 5, (min_value, max_value));
+
+    let mut phi: f32 = 0.;
 
     while !rl.window_should_close() {
         let mut d = rl.begin_drawing(&thread);
         d.clear_background(Color::BLACK);
 
-        matrix.visualize(&mut d, min_value, max_value);
+        matrix
+            .map(|x| x * (phi * PI).sin())
+            .visualize(&mut d, min_value, max_value);
+
+        phi += 0.01;
     }
 }
